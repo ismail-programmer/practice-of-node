16521802caa8455c833d5ffa299c08859abc1ec6
Ismail <lord28062000@gmail.com>
2019-10-04 23:33:08 +0500

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var name = superheroes.random();
+console.log(name);
+
+
+var superhevillans = require("supervillains ");
+var name1 = superheroes.random();
+console.log(name1);
